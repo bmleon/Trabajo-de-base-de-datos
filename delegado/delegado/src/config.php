cf5cb73554f517889d46675d35aaa28d6618a5db
Belén María León <75305764+bmleon@users.noreply.github.com>
2022-11-24 19:05:24 +0100

    Add files via upload

---
+++ delegado/delegado/src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('MYSQL_HOST', '172.31.87.170');
+define('MYSQL_DATABASE', 'delegado');
+define('MYSQL_USER', 'delegado');
+define('MYSQL_PASSWORD', 'Root1234$');
+
+$mysqli = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
+
+?>
